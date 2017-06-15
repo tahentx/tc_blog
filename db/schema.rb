68325a4f78d6b8520dab225b932109a568d43698
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-15 09:34:55 -0500

    update users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613190045) do
+ActiveRecord::Schema.define(version: 20170615142339) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20170613190045) do
     t.datetime "updated_at", null: false
     t.string "uuid"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
   end
 
 end
