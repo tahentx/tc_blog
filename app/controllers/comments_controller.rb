35a651668df5340b672388efd28b771d451cc974
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-16 16:07:02 -0500

    debug delete comments functionality, changed code in view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,13 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
   def create
-    @article = Article.find(params[:article_id])
+    @article = Article.find_by(uuid: params[:id])
     @comment = @article.comments.create(comment_params).valid?
     redirect_to article_path(@article.uuid)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
+    @article = Article.find_by(uuid: params[:id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
